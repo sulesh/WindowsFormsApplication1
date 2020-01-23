f445bba517735981083561c101a9dc94dcae49ed
sulesh.shukla2009@gmail.com <sulesh.shukla2009@gmail.com>
2020-01-23 16:27:36 +0530

    Add project files.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Math;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Console.WriteLine("Hi Sulesh");
+
+            MessageBox.Show("SS");
+
+            Program1 xxx = new Program1();
+            xxx.abc(5);
+
+                        
+        }
+
+       public class Program1
+        {
+           public  void abc(int a)
+            {
+                Console.WriteLine(a);
+            }
+          
+            
+        }
+
+   
+
+    }
+}
